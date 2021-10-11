fb5e9c5875d92ac6017e15e8011892341d8911ee
antras1 <thesolirian@gmail.com>
2021-10-11 16:37:14 +0200

    version test

--- src/Controller/controller1.php
+++ src/Controller/controller1.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
-use App\Entity\Resultats;
+use App\Entity\Echantillon;
 use App\Entity\Resultats;
 use Doctrine\ORM\EntityManagerInterface;
 use http\Env\Request;
@@ -12,22 +12,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class controller1 extends AbstractController{
-#[Route(path:'ajout')]
 
-public function Ajout_P(Request $request, EntityManagerInterface $entityManager){
-    //$o = new patient(@data);
+    #[Route(path:'ajout',methods: ['POST'])]
+
+public function ajout_P (Request $request, EntityManagerInterface $entityManager, $nouvPatient): Response
+    {
     $data=json_decode($request->getContent());
-    $nouvPatient = $entityManager -> getRepository(Patient::class)->find();
-    $nouvPatient->nomP = $data->nom;
-    $nouvPatient->prenomP = $data->prenom;
-    $nouvPatient->Genre = $data->genre;
-    $nouvPatient->age = $data->age;
-    $nouvPatient->GrpSang = $data->grpsang;
-    $nouvPatient->N_SecSoc = $data->SecSoc;
+    $o = new patient(
+        $nouvPatient->nomP = $data->nom,
+        $nouvPatient->prenomP = $data->prenom,
+        $nouvPatient->Genre = $data->genre,
+        $nouvPatient->age = $data->age,
+        $nouvPatient->GrpSang = $data->grpsang,
+        $nouvPatient->N_SecSoc = $data->SecSoc);
 
-    $em->persist($o); //sert à $em de se souvenir de l'objet $o.
-    $em-> flush();
-    $bidule = $em->getRepository(test::class)->find(1);
+    $entityManager->persist($o);
+    $entityManager-> flush();
+    $bidule = $entityManager->getRepository(Patient::class)->findall();
     return new Response($bidule->getNom());
     return $this->render();
 }
